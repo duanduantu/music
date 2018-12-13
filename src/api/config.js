88088dd7ed0e220a9082dfc6b50b95b897548e0b
dlj <827532234@qq.com>
2018-12-13 22:40:20 +0800

    首页轮播图

---
+++ src/api/config.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Administrator on 2018/12/13 0013.
+ */
+
+export const commonParams = {
+  g_tk: 5381,
+  inCharset: 'utf-8',
+  outCharset: 'utf-8',
+  notice: 0,
+  format: 'jsonp'
+}
+
+export const options = {
+  param: 'jsonpCallback'
+}
+
+export const ERR_OK = 0
