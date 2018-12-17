3faaa96f3a32801124ac8eed83be86cceae61cea
dlj <827532234@qq.com>
2018-12-17 17:03:19 +0800

    后端代理接口

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -11,8 +11,18 @@
         </slider>
       </div>
       <div class="recommend-list">
-        <h1>热门歌单推荐</h1>
-        <ul></ul>
+        <h1 class="list-title">热门歌单推荐</h1>
+        <ul>
+          <li v-for="(item, index) in discList" class="item" :key="index">
+            <div class="icon">
+              <img width="60" height="60" :src="item.imgurl"/>
+            </div>
+            <div class="text">
+              <h2 class="name" v-html="item.creator.name"></h2>
+              <p class="desc" v-html="item.dissname"></p>
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
@@ -20,13 +30,14 @@
 
 <script type="text/ecmascript-6">
 import Slider from 'base/slider/slider'
-import {getRecommend} from 'api/recommend'
+import {getRecommend, getDiscList} from 'api/recommend'
 import {ERR_OK} from 'api/config'
 
 export default {
   data () {
     return {
-      recommends: []
+      recommends: [],
+      discList: []
     }
   },
   components: {
@@ -34,12 +45,21 @@ export default {
   },
   created () {
     this._getRecommend()
+    this._getDiscList()
   },
   methods: {
     _getRecommend () {
       getRecommend().then((res) => {
         if (res.code === ERR_OK) {
           this.recommends = res.data.slider
+        }
+      })
+    },
+    _getDiscList () {
+      getDiscList().then((res) => {
+        if (res.code === ERR_OK) {
+          console.log(res.data.list)
+          this.discList = res.data.list
         }
       })
     }
