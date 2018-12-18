c5708c6840dae2f7a6c0712b66d6ab5be3350fc7
dlj <827532234@qq.com>
2018-12-18 16:30:24 +0800

    添加歌手页

---
+++ src/base/listview/listview.vue
@@ -0,0 +1,50 @@
+<template>
+  <scroll class="listview" :data="data">
+    <ul>
+      <li v-for="(group, index) in data" class="list-group" :key="index">
+        <h2 class="list-group-title">{{group.title}}</h2>
+        <ul>
+          <li v-for="item in group.items" class="list-group-item" :key="item.id">
+            <img class="avatar" :src="item.avatar"/>
+            <span class="name">{{item.name}}</span>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </scroll>
+</template>
+
+<script type="text/ecmascript-6">
+import Scroll from 'base/scroll/scroll'
+export default {
+  props: {
+    data: {
+      type: Array,
+      default: null
+    }
+  },
+  components: {
+    Scroll
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  .listview
+    width: 100%
+    height: 100%
+    overflow: hidden
+    background: $color-background
+    .list-group
+      .list-group-title
+        height: 30px
+        line-height: 30px
+        padding-left: 20px
+        font-size: $font-size-small
+        color: $color-text-l
+        background: $color-highlight-background
+      .list-group-item
+        display: flex
+        align-items: center
+</style>
