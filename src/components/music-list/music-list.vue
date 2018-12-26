aa13dd50c2275ff067585c95c19168ed403dda3d
dlj <827532234@qq.com>
2018-12-26 20:55:15 +0800

    添加随机播放按钮和loading

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="music-list">
-    <div class="back">
+    <div class="back" @click="back">
       <i class="icon-back"></i>
     </div>
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
+      <div class="play-wrapper">
+        <div class="play" ref="playBtn" v-show="songs.length>0">
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
       <div class="filter" ref="filter"></div>
     </div>
     <div class="bg-layer" ref="layer"></div>
@@ -19,6 +25,9 @@
       <div class="song-list-wrapper">
         <song-list :songs="songs"></song-list>
       </div>
+      <div class="loading-container"  v-show="!songs.length">
+        <loading></loading>
+      </div>
     </scroll>
   </div>
 </template>
@@ -26,6 +35,7 @@
 <script type="text/ecmascript-6">
 import Scroll from 'base/scroll/scroll'
 import SongList from 'base/song-list/song-list'
+import Loading from 'base/loading/loading'
 import {prefixStyle} from 'common/js/dom'
 const RESERVED_HEIGHT = 40
 const transform = prefixStyle('transform')
@@ -38,7 +48,8 @@ export default {
   },
   components: {
     Scroll,
-    SongList
+    SongList,
+    Loading
   },
   props: {
     bgImage: {
@@ -62,6 +73,9 @@ export default {
   methods: {
     scroll (pos) {
       this.scrollY = pos.y
+    },
+    back () {
+      this.$router.back()
     }
   },
   watch: {
@@ -71,6 +85,7 @@ export default {
       let zIndex = 0
       let blur = 0
       const percent = Math.abs(newY / this.imageHeight)
+      // 向下滚动
       if (newY > 0) {
         scale = 1 + percent
         zIndex = 10
@@ -79,13 +94,16 @@ export default {
       }
       this.$refs.layer.style[transform] = `translate3d(0,${translateY}px,0)`
       this.$refs.filter.style[backdrop] = `blur(${blur}px)`
+      // 滚动到顶部
       if (newY < this.minTranslateY) {
         zIndex = 10
         this.$refs.bgImage.style.paddingTop = 0
         this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+        this.$refs.playBtn.style.display = 'none'
       } else {
         this.$refs.bgImage.style.paddingTop = '70%'
         this.$refs.bgImage.style.height = 0
+        this.$refs.playBtn.style.display = ''
       }
       this.$refs.bgImage.style[transform] = `scale(${scale})`
       this.$refs.bgImage.style.zIndex = zIndex
@@ -144,6 +162,29 @@ export default {
       padding-top: 70%
       transform-origin: top
       background-size: cover
+      .play-wrapper
+        position: absolute
+        bottom: 20px
+        width: 100%
+        z-index: 50
+        .play
+          width: 135px
+          margin: 0 auto
+          padding: 7px 0
+          text-align: center
+          border: 1px solid $color-theme
+          color: $color-theme
+          border-radius: 100px
+          font-size: 0
+          .icon-play
+            display: inline-block
+            vertical-align: middle
+            margin-right: 6px
+            font-size: $font-size-medium-x
+          .text
+            display: inline-block
+            vertical-align: middle
+            font-size: $font-size-small
       .filter
         position: absolute
         top: 0
@@ -163,4 +204,9 @@ export default {
       background: $color-background
       .song-list-wrapper
         padding: 20px 30px
+      .loading-container
+        position: absolute
+        width: 100%
+        top: 50%
+        transform: translateY(-50%)
 </style>
