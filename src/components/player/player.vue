b8d3d29f3d3fa0ffba2a70ab05a81670772dcbee
dlj <827532234@qq.com>
2019-01-03 21:52:12 +0800

    添加样式和动画

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,21 +1,22 @@
 <template>
   <div class="player" v-show="playList.length>0">
-    <div class="normal-player" v-show="fullScreen">
+    <transition name="normal">
+      <div class="normal-player" v-show="fullScreen">
       <div class="background">
-        <img width="100%" height="100%"/>
+        <img width="100%" height="100%" :src="currentSong.image"/>
       </div>
       <div class="top">
-        <div class="back">
+        <div class="back" @click="back">
           <i class="icon-back"></i>
         </div>
-        <h1 class="title"></h1>
-        <h2 class="subtitle"></h2>
+        <h1 class="title" v-html="currentSong.name"></h1>
+        <h2 class="subtitle" v-html="currentSong.singer"></h2>
       </div>
       <div class="middle">
         <div class="middle-l">
           <div class="cd-wrapper">
             <div class="cd">
-              <img class="image"/>
+              <img class="image" :src="currentSong.image"/>
             </div>
           </div>
         </div>
@@ -40,37 +41,53 @@
         </div>
       </div>
     </div>
-    <div class="mini-player" v-show="!fullScreen">
+    </transition>
+    <transition name="mini">
+    <div class="mini-player" v-show="!fullScreen" @click="open">
       <div class="icon">
-        <img width="40" height="40"/>
+        <img width="40" height="40" :src="currentSong.image"/>
       </div>
       <div class="text">
-        <h2 class="name"></h2>
-        <p class="desc"></p>
+        <h2 class="name" v-html="currentSong.name"></h2>
+        <p class="desc" v-html="currentSong.singer"></p>
       </div>
       <div class="control"></div>
       <div class="control">
         <i class="icon-playlist"></i>
       </div>
     </div>
+    </transition>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-import {mapGetters} from 'vuex'
+import {mapGetters, mapMutations} from 'vuex'
 
 export default {
   computed: {
     ...mapGetters([
       'fullScreen',
-      'playList'
+      'playList',
+      'currentSong'
     ])
+  },
+  methods: {
+    back () {
+      this.setFullScreen(false)
+    },
+    open () {
+      this.setFullScreen(true)
+    },
+    ...mapMutations({
+      setFullScreen: 'SET_FULL_SCREEN'
+    })
   }
 }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
   .player
     .normal-player
       position: fixed
@@ -80,6 +97,110 @@ export default {
       left: 0
       z-index: 150
       background: $color-background
+      .background
+        position: absolute
+        left: 0
+        top: 0
+        width: 100%
+        height: 100%
+        z-index: -1
+        opacity: 0.6
+        filter: blur(20px)
+      .top
+        position: relative
+        margin-bottom: 25px
+        .back
+          position: absolute
+          top: 0
+          left: 6px
+          z-index: 50
+          .icon-back
+            display: block
+            padding: 9px
+            font-size: $font-size-large-x
+            color: $color-theme
+            transform: rotate(-90deg)
+        .title
+          width: 70%
+          margin: 0 auto
+          line-height: 40px
+          text-align: center
+          no-wrap()
+          font-size: $font-size-large
+          color: $color-text
+        .subtitle
+          line-height: 20px
+          text-align: center
+          font-size: $font-size-medium
+          color: $color-text
+      .middle
+        position: fixed
+        width: 100%
+        top: 80px
+        bottom: 170px
+        white-space: nowrap
+        font-size: 0
+        .middle-l
+          display: inline-block
+          vertical-align: top
+          position: relative
+          width: 100%
+          height: 0
+          padding-top: 80%
+          .cd-wrapper
+            position: absolute
+            left: 10%
+            top: 0
+            width: 80%
+            height: 100%
+            .cd
+              width: 100%
+              height: 100%
+              box-sizing: border-box
+              border: 10px solid rgba(255, 255, 255, 0.1)
+              border-radius: 50%
+              .image
+                position: absolute
+                left: 0
+                top: 0
+                width: 100%
+                height: 100%
+                border-radius: 50%
+      .bottom
+        position: absolute
+        bottom: 50px
+        width: 100%
+        .operators
+          display: flex
+          align-items: center
+          .icon
+            flex: 1
+            color: $color-theme
+            &.disable
+              color: $color-theme-d
+            i
+              font-size: 30px
+          .i-left
+            text-align: right
+          .i-center
+            padding: 0 20px
+            text-align: center
+            i
+              font-size: 40px
+          .i-right
+            text-align: left
+          .icon-favorite
+            color: $color-sub-theme
+      &.normal-enter-active, &.normal-leave-active
+        transition: all 0.4s
+        .top, .bottom
+          transition: all 0.4s cubic-bezier(0.86, 0.18, 0.82, 1.32)
+      &.normal-enter, &.normal-leave-to
+        opacity: 0
+        .top
+          transform: translate3d(0, -100px, 0)
+        .bottom
+          transform: translate3d(0, 100px, 0)
     .mini-player
       display: flex
       align-items: center
@@ -90,5 +211,47 @@ export default {
       width: 100%
       height: 60px
       background: $color-highlight-background
+      &.mini-enter-active, &.mini-leave-active
+        transition: all 0.4s
+      &.mini-enter, &.mini-leave-to
+        opacity: 0
+      .icon
+        flex: 0 0 40px
+        width: 40px
+        padding: 0 10px 0 20px
+        img
+          border-radius: 50%
+          &.play
+            animation: rotate 10s linear infinite
+          &.pause
+            animation-play-state: paused
+      .text
+        display: flex
+        flex-direction: column
+        justify-content: center
+        flex: 1
+        line-height: 20px
+        overflow: hidden
+        .name
+          margin-bottom: 2px
+          no-wrap()
+          font-size: $font-size-medium
+          color: $color-text
+        .desc
+          no-wrap()
+          font-size: $font-size-small
+          color: $color-text-d
+      .control
+        flex: 0 0 30px
+        width: 30px
+        padding: 0 10px
+        .icon-play-mini, .icon-pause-mini, .icon-playlist
+          font-size: 30px
+          color: $color-theme-d
+        .icon-mini
+          font-size: 32px
+          position: absolute
+          left: 0
+          top: 0
 
 </style>
